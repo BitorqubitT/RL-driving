615184b1e41b9fd7564fb884605d81586d18276f
BitorqubitT <thierryars2@hotmail.com>
2024-07-29 22:23:21 +0200

    finishing up training loop

--- main_with_col.py
+++ main_with_col.py
@@ -2,18 +2,23 @@ import math
 import pygame
 import pygame.freetype
 import torch
+import random
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 from pygame.sprite import Sprite
 import numpy as np
 from utils.ui import play_level
 from utils.helper import store_replay
 from utils.helper import read_replay
+from dqn import DQN
+from dqn import select_action
+from dqn import plot_durations
+from dqn import ReplayMemory
 
 """
 TODO:
-# Implement a training loop for rl
 # Reward and penalty
 # Maybe make it possible to keep going after one lap
 # Can also implement this after network
@@ -374,13 +379,69 @@ if __name__ == "__main__":
             TAU = 0.005
             LR = 1e-4
 
-            n_actions = # set action space
+            #TODO: check if this is correct
+            n_actions = 8
 
-            # What do we put in info?
-            #state, info = env.reset()
+            # TODO: What do we put in info?
+            state, info = env.reset()
+            n_observations = len(state)
 
+            policy_net = DQN(n_observations, n_actions).to(device)
+            target_net = DQN(n_observations, n_actions).to(device)
+            target_net.load_state_dict(policy_net.state_dict())
 
+            optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+            memory = ReplayMemory(10000)
 
+            steps_done = 0
+
+            episode_durations = []
+
+            # Move this?
+            def optimize_model():
+                if len(memory) < BATCH_SIZE:
+                    return
+                transitions = memory.sample(BATCH_SIZE)
+                # detailed explanation). This converts batch-array of Transitions
+                # to Transition of batch-arrays.
+                batch = Transition(*zip(*transitions))
+
+                # Compute a mask of non-final states and concatenate the batch elements
+                # (a final state would've been the one after which simulation ended)
+                non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                                    batch.next_state)), device=device, dtype=torch.bool)
+                non_final_next_states = torch.cat([s for s in batch.next_state
+                                                            if s is not None])
+                state_batch = torch.cat(batch.state)
+                action_batch = torch.cat(batch.action)
+                reward_batch = torch.cat(batch.reward)
+
+                # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+                # columns of actions taken. These are the actions which would've been taken
+                # for each batch state according to policy_net
+                state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+                # Compute V(s_{t+1}) for all next states.
+                # Expected values of actions for non_final_next_states are computed based
+                # on the "older" target_net; selecting their best reward with max(1).values
+                # This is merged based on the mask, such that we'll have either the expected
+                # state value or 0 in case the state was final.
+                next_state_values = torch.zeros(BATCH_SIZE, device=device)
+                with torch.no_grad():
+                    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1).values
+                # Compute the expected Q values
+                expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+                # Compute Huber loss
+                criterion = nn.SmoothL1Loss()
+                loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+                # Optimize the model
+                optimizer.zero_grad()
+                loss.backward()
+                # In-place gradient clipping
+                torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+                optimizer.step()
 
             while current_game:
                 
@@ -391,27 +452,47 @@ if __name__ == "__main__":
                         current_game = False
 
             # let the agent choose action
+                action = select_action(state)
+                
+                # Check expected types etc
+                observation, reward, terminated, truncated = env.step(action.item())
+                
+                reward = torch.tensor([reward], device=device)
+                done = terminated or truncated
 
-            # use action as input to the game
-            # x.step(action)
+                if terminated:
+                    next_state = None
+                else:
+                    next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqeeze(0)
 
-            # somehow return observation, reward, current_game (is this corrrect?)
-            # observation can be image around the car (can change this)
-            # obeservation can be input from sensors
+                memory.push(state, action, next_state, reward)
 
-            # check if we get a reward in x ticks
-            # if we dont the end this run/episode
+                state = next_state
 
-            # put this output in the network.
-            # pUT SAFEGUARD HERE    
-            # Save output from the netowrk
-            # scores.append(something)
+                optimize_model()
 
-            # update network parms
+                target_net_state_dict = target_net.state_dict()
+                policy_net_state_dict = policy_net.state_dict()
+
+                for key in policy_net_state_dict:
+                    target_net_state_dict[key] = policy_net_state_dict[key]*TAU +
+                    target_net_state_dict[key] * (1-TAU)
+                
+                target_net.load_state_dict(target_net_state_dict)
+
+                if done:
+                    episode_durations.append(t + 1)
+                    plot_durations()
+                    break
         
-            all_replays.append(env.history)
-            replay_per_run = store_replay(all_replays)
-    
+            #all_replays.append(env.history)
+            #replay_per_run = store_replay(all_replays)
+            
+            print("Finished")
+            plot_durations(show_result=True)
+            plt.ioff()
+            plt.show()
+
     elif MODE == "player":
         current_game = True
         
