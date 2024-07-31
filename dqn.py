353837f19f9613b63ea438a2d3211e962f287ff2
BitorqubitT <thierryars2@hotmail.com>
2024-07-31 23:32:25 +0200

    adepted to non gym env

--- dqn.py
+++ dqn.py
@@ -1,75 +1,114 @@
-from gym import Env
-from gym.spaces import Discrete, Box
-import numpy as np
+import math
 import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
 
-class ShowerEnv(Env):
-    def __init__(self):
-        # Actions we can take, down, stay, up
-        self.action_space = Discrete(3)
-        # Temperature array
-        self.observation_space = Box(low=np.array([0]), high=np.array([100]))
-        # Set start temp
-        self.state = 38 + random.randint(-3,3)
-        # Set shower length
-        self.shower_length = 60
-        
-    def step(self, action):
-        # Apply action
-        # 0 -1 = -1 temperature
-        # 1 -1 = 0 
-        # 2 -1 = 1 temperature 
-        self.state += action -1 
-        # Reduce shower length by 1 second
-        self.shower_length -= 1 
-        
-        # Calculate reward
-        if self.state >=37 and self.state <=39: 
-            reward =1 
-        else: 
-            reward = -1 
-        
-        # Check if shower is done
-        if self.shower_length <= 0: 
-            done = True
-        else:
-            done = False
-        
-        # Apply temperature noise
-        #self.state += random.randint(-1,1)
-        # Set placeholder for info
-        info = {}
-        
-        # Return step information
-        return self.state, reward, done, info
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
-    def render(self):
-        # Implement viz
-        pass
+#https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+
+# if GPU is to be used
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "mps" if torch.backends.mps.is_available() else
+    "cpu"
+)
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
     
-    def reset(self):
-        # Reset shower temperature
-        self.state = 38 + random.randint(-3,3)
-        # Reset shower time
-        self.shower_length = 60 
-        return self.state
-    
-
-env = ShowerEnv()
 
 
-env.observation_space.sample()
+# BATCH_SIZE is the number of transitions sampled from the replay buffer
+# GAMMA is the discount factor as mentioned in the previous section
+# EPS_START is the starting value of epsilon
+# EPS_END is the final value of epsilon
+# EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
+# TAU is the update rate of the target network
+# LR is the learning rate of the ``AdamW`` optimizer
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TAU = 0.005
+LR = 1e-4
+
+steps_done = 0
+
+def select_action(state, env, policy_net):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return the largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            # TODO: For now we use policy_net as input for select action.
+            return policy_net(state).max(1).indices.view(1, 1)
+    else:
+        # Pytorch uses env.action_space.sample because they have another class.
+        #return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
+        return torch.tensor([[env.sample()]], device=device, dtype=torch.long)
 
 
-episodes = 10
-for episode in range(1, episodes+1):
-    state = env.reset()
-    done = False
-    score = 0 
-    
-    while not done:
-        #env.render()
-        action = env.action_space.sample()
-        n_state, reward, done, info = env.step(action)
-        score+=reward
-    print('Episode:{} Score:{}'.format(episode, score))
+episode_durations = []
+
+
+def plot_durations(show_result=False):
+    plt.figure(1)
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    if show_result:
+        plt.title('Result')
+    else:
+        plt.clf()
+        plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    # Take 100 episode averages and plot them too
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
