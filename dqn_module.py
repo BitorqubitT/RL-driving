ced7e3733284bf067ce0cbda41dfbdd9c0476b6c
BitorqubitT <thierryars2@hotmail.com>
2024-09-03 22:08:56 +0200

    seperate dqn module

---
+++ dqn_module.py
@@ -0,0 +1,129 @@
+import math
+import torch
+import random
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from collections import namedtuple, deque
+import math
+import random
+from collections import namedtuple, deque
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+# TODO:
+# https://github.com/CodeAndAction/DDQN-Car-Racing/blob/main/ddqn_keras.py
+# split better, create class dqn-racer or something add stuff 
+
+Transition = namedtuple('Transition',
+        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class DQNagent():
+
+    def __init__(self, device, batch_size, n_observations, n_actions, gamma, eps_end, eps_start, decay, lr, tau):
+        self.policy_net = DQN(n_observations, n_actions).to(device)
+        self.target_net = DQN(n_observations, n_actions).to(device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.device = device
+        self.batch_size = batch_size
+        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=lr, amsgrad=True)
+        self.gamma = gamma
+        self.memory = ReplayMemory(10000)
+        self.eps_end = eps_end
+        self.eps_start = eps_start
+        self.eps_decay = decay
+        self.steps_done = 0
+        self.tau = tau
+
+    def push_memory(self, state, action, next_state, reward):
+        self.memory.push(state, action, next_state, reward)
+
+    def select_action(self, state, env):
+        #maybe remove global
+        global steps_done
+        sample = random.random()
+        eps_threshold = self.eps_end + (self.eps_start - self.eps_end) * \
+            math.exp(-1. * self.steps_done / self.eps_decay)
+        self.steps_done += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                # t.max(1) will return the largest column value of each row.
+                # second column on max result is index of where max element was
+                # found, so we pick action with the larger expected reward.
+                return self.policy_net(state).max(1).indices.view(1, 1)
+        else:
+            return torch.tensor([[env.sample()]], device=self.device, dtype=torch.long)
+
+    def update_param(self):
+        # State dictionary is mapping from weights to layers
+        target_net_state_dict = self.target_net.state_dict()
+        policy_net_state_dict = self.policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key] * self.tau + target_net_state_dict[key] * (1 - self.tau)
+        self.target_net.load_state_dict(target_net_state_dict)
+
+    def optimize_model(self):
+        if len(self.memory) < self.batch_size:
+            return
+        
+        transitions = self.memory.sample(self.batch_size)
+
+        batch = Transition(*zip(*transitions))
+
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                            batch.next_state)), device=self.device, dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                                    if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+        next_state_values = torch.zeros(self.batch_size, device=self.device)
+        
+        with torch.no_grad():
+            next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1).values
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+
+        #criterion = nn.SmoothL1Loss()
+        criterion = nn.MSELoss()
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer.step()
+
+
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 256)
+        self.layer2 = nn.Linear(256, 256)
+        self.layer3 = nn.Linear(256, n_actions)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
