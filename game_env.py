0d30951103dc8a74168badb931459d6984cb1a6e
BitorqubitT <thierryars2@hotmail.com>
2024-09-30 19:10:25 +0200

    fix commit issues

--- game_env.py
+++ game_env.py
@@ -1,18 +1,20 @@
 import math
 import pygame
 import pygame.freetype
-import torch
 import random
 import ast
 from pygame.sprite import Sprite
 import numpy as np
+from utils.helper import rotate_point
+from utils.helper import calculate_angle
 from utils.ui import play_level
+from utils.helper import is_point_on_line
 
-
-# Window size
-WINDOW_WIDTH    = 1920
-WINDOW_HEIGHT   = 1080
-WINDOW_SURFACE  = pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE
+"""
+    This contains the game environment for a simple car racing game.
+    The graphics are seperated from the game logic.
+    TODO: Check how to write good intro, name the different classes?
+"""
 
 BLUE = (106, 159, 181)
 WHITE = (255, 255, 255)
@@ -20,14 +22,21 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
 class Car(Sprite):
+    """
+        A class to represent a car.
+    """
 
     def __init__(self, car_image:str, x: float, y: float, player_type):
+        """
+        Args:
+            car_image (str): location of the car image
+            x (float): starting x position
+            y (float): starting y position
+            player_type (_type_): check if we play as ai, player or viewer
+        """
         super().__init__()
-        #TODO: clean this angle stuff
-        
         self.min_angle = math.radians(1)
         self.start_pos = (x, y)
-        self.reversing = False
         self.heading   = 0
         self.speed     = 0
         self.velocity  = pygame.math.Vector2(0, 0)
@@ -70,14 +79,6 @@ class Car(Sprite):
             (half_width, half_height)
         ]
 
-        # Function to rotate a point around the center
-        def rotate_point(px, py, angle):
-            cos_angle = math.cos(angle)
-            sin_angle = math.sin(angle)
-            x_new = px * cos_angle - py * sin_angle
-            y_new = px * sin_angle + py * cos_angle
-            return x_new, y_new
-
         # Calculate the rotated corner positions
         rotated_corners = [rotate_point(px, py, angle_radians) for px, py in corners]
 
@@ -88,11 +89,6 @@ class Car(Sprite):
         translated_corners = [translated_corners[1], translated_corners[3], translated_corners[2], translated_corners[0]]
 
         return translated_corners
-
-    def calculate_angle(self, source, target):
-        velocity = (target[0] - source[0], target[1] - source[1])
-        angle = math.atan2(velocity[1], velocity[0])
-        return angle
     
     def calculate_hitboxes(self) -> list:
         # Front, front_T, back_T, back_b       
@@ -106,7 +102,7 @@ class Car(Sprite):
         hitbox_points = []
         for distance, point_pair in enumerate(hitboxes):
             #TODO: REPLACE GARBAGE CODE
-            angle = self.calculate_angle(point_pair[0], point_pair[1])
+            angle = calculate_angle(point_pair[0], point_pair[1])
             # Set range based on distance between points
             # We know this because of the size of the car.
             if distance == 0 or distance == 2:
@@ -154,6 +150,7 @@ class Car(Sprite):
         if (abs(self.speed) < 0.1):
             self.speed = 0
 
+    # TODO: make this faster
     def _cast_ray(self, arr, angle_offset) -> None:
         x, y = 0, 0
         heading = self.heading + angle_offset
@@ -170,7 +167,6 @@ class Car(Sprite):
 
     def distance_to_walls(self, walls) -> list:
         ray_angles = [0, 1.570, 4.712, 0.524, 5.76, 3.142, 3.67, 2.618]
-        #ray_angles = [0, 0.524, 5.76]
         distances = []
         all_position = []
         for i, angle in enumerate(ray_angles):
@@ -182,24 +178,7 @@ class Car(Sprite):
         return distances, all_position
     
     def check_checkpoint(self, checkpoints) -> bool:
-        def is_point_on_line(p1, p2, p):
-            # Calculate the cross product of vectors (p1 -> p) and (p1 -> p2)
-            dxc = p[0] - p1[0]
-            dyc = p[1] - p1[1]
-            dxl = p2[0] - p1[0]
-            dyl = p2[1] - p1[1]
-            cross = dxc * dyl - dyc * dxl
-
-            # If the cross product is zero, the point is on the line
-            if cross <= 10:
-                return False
-
-            # Check if the point is within the bounds of the line segment
-            if abs(dxl) >= abs(dyl):
-                return p1[0] <= p[0] <= p2[0] if dxl > 0 else p2[0] <= p[0] <= p1[0]
-            else:
-                return p1[1] <= p[1] <= p2[1] if dyl > 0 else p2[1] <= p[1] <= p1[1]
-
+        #TODO: maybe remove thie function all together.
         return is_point_on_line(checkpoints[0][0], checkpoints[0][1], (round(self.position[0]), round(self.position[1])))
 
     def update(self) -> None:
@@ -218,33 +197,28 @@ class Car(Sprite):
         self.heading = 0
 
     def action(self, keys) -> None:
-        # change this functiono depending on player or pc?
-        # if pc just give int value between 1->8 actions?
-        if self.player_type == "player":
+        if self.player_type == "plaayer":
             if keys[pygame.K_UP]:
                 self._accelerate(1.0)
             if keys[pygame.K_DOWN]:
                 self._brake()
             if self.speed != 0:
                 if keys[pygame.K_RIGHT]:
-                    print("aihwdowihd")
-                    self._turn(12)
+                    self._turn(6)
                 if keys[pygame.K_LEFT]:
-                    self._turn(-12)
+                    self._turn(-6)
         else:
-            # TODO: Does it make sense to do both at the same time?
+            #if keys == 0:
+              #  pass
             if keys == 0:
-                pass
-            elif keys == 4:
                 self._accelerate(1.0)
             elif keys == 2:
-                self._turn(10)
+                self._turn(6)
             elif keys == 3:
                 self._brake()
             elif keys == 1:
-                self._turn(-10)
+                self._turn(-6)
 
-# create one superclass i think
 class Level(Sprite):
     def __init__(self, image: str, x: int, y: int):
         Sprite.__init__(self)
@@ -252,60 +226,51 @@ class Level(Sprite):
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.center = (x, y) 
-# add move to player class maybe?
 
 class Environment():
     """load and update the game, take in actions, keep score"""
-    def __init__(self, mode):
-        self.width, self.height = 1920, 1080
-        self.action_space = np.array([0, 1, 2, 3, 4])
-        #self.action_space = np.array([0, 1, 2])
+    #TODO: Give map with params
+    def __init__(self, mode: str, map: str, start_pos: tuple):
+        #TODO maybe change with height we never change these
+        # Should we have them here?
+        self.action_space = np.array([0, 1, 2, 3])
         self.observation_space = None
         self.reward = 0
         self.history = []
         self.mode = mode
-        self.walls = self._get_walls("track_info/track_1.csv")
+        self.map = map 
+        self.walls = self._get_walls()
         self.checkpoints = self._set_checkpoints()
         self.extracheckp = self._set_checkpoints()
+        
         if mode != "ai":
-            self.width, self.height = 1920, 1080
-            pygame.init()
-            pygame.display.set_caption("Car sim :)")
-            self.window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_SURFACE)
-            self.background = pygame.image.load("assets/background2.png")
-            self.car_group = pygame.sprite.Group()
-            self.track_group = self._load_obstacles()
-            # TODO: maybe add distance to walls here
+            self._initialize_pygame()
         self.reset()
 
-    # Dirty way to visualise a replay based on positions only
-    def set_pos(self, pos) -> None:
-        self.car.position = pygame.math.Vector2(pos[0], pos[1])
-
-    def set_angle_manual(self, angle) -> None:
-        image_index = int(angle / self.car.min_angle) % len(self.car.rot_img)
-        if (self.car.image != self.car.rot_img[ image_index ]):
-            x,y = self.car.rect.center
-            self.car.image = self.car.rot_img[ image_index ]
-            self.car.rect  = self.car.image.get_rect()
-            self.car.rect.center = (x,y)
-            # need to update mask or collision will use og image
-            self.mask = pygame.mask.from_surface(self.car.image)
+    def _initialize_pygame(self):
+        """Initialize pygame components."""
+        pygame.init()
+        pygame.display.set_caption("Car sim :)")
+        self.window = pygame.display.set_mode((1920, 1080), pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
+        self.background = pygame.image.load("assets/background2.png")
+        self.car_group = pygame.sprite.Group()
+        self.track_group = self._load_obstacles()
 
     def reset(self) -> None:
         # TODO: Cleaner way to solve this?
-        if self.mode == "player" or self.mode == "view":
+        if self.mode == "player":
             # track 2 950, 100
             # track 1 750, 200
             self.car = Car("assets/car12.png", 550, 125, self.mode)
             self.car_group.add(self.car)
-            self.reward = 0
         else:
             self.car = Car("assets/car12.png", 550, 125, "ai")
         distances, xx = self.car.distance_to_walls(self.walls)
         self.checkpoints = self._set_checkpoints()
+        self.extracheckp = self._set_checkpoints()
         self.reward = 0
         self.history = []
+        #TODO: check this, pretty sure this is used for normalisation
         distances.append(self.car.speed/3)
 
         return distances
@@ -314,10 +279,10 @@ class Environment():
         return random.choice(self.action_space)
 
     def step(self, keys) -> None:
-        #self.car.action(keys)
-        #print(self.car.position)
-        # If I give the position directly it just updates lol
-        # Its a reference to the object
+        #TODO: WE ADDED SPEED TO DISTNCES AS A REWARD, clean this up
+
+        # Using reward like this is pretty weird
+        # Shouldn't we get a reward for not crashing and just advancing in the right direction>?
         self.reward = 0
         # TODO: CLEAN THIS UP
         self.history.append([self.car.position[0], self.car.position[1], self.car.heading, self.car.speed, self.reward, keys])
@@ -328,7 +293,8 @@ class Environment():
         # Get hitbox positions
         indexlist = self.car.calculate_hitboxes()
         indexlisttranspose = np.array(indexlist).T.tolist()
-        
+
+        #TODO: use ___ for unused var? 
         distances, xx = self.car.distance_to_walls(self.walls)
         # Create a list with map values at hitbox position.
         # AKA check if wall was found
@@ -339,24 +305,26 @@ class Environment():
             self.reward -= 1
             self.car.reset()
             #TODO: Add reward or other stats, maybe?
+            #TODO: remove the weird distance append
             distances.append(self.car.speed/3)
             return distances, self.reward, True, False
 
         if len(self.checkpoints) == 0:
-            self.checkpoints = self.extracheckp
+            self.reward += 40
+            self.checkpoints = self._set_checkpoints()
 
         # Calculate reward
+        # TODO: Other way of reloading checkpoints
         if self.car.check_checkpoint(self.checkpoints):
             self.reward += 1
             self.checkpoints.pop(0)
 
         # TODO: make this accesible through the environment by adding it at init?
+        
         distances.append(self.car.speed/3)
-
         return distances, self.reward, False, False
         
     def render(self) -> None:
-        #pygame.time.delay(10)
         #self.clock = pygame.time.Clock()
         #TODO: this one seems extra
         self.window.blit(self.background, (0, 0))
@@ -380,19 +348,20 @@ class Environment():
 
     # What is returned?
     def _load_obstacles(self) -> None:
-        self.track = Level("assets/track 1.png", WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+        self.track = Level("assets/" + self.map + ".png", 1920//2, 1080//2)
         self.track_group = pygame.sprite.Group()
         self.track_group.add(self.track)
         return self.track_group
     
     def _set_checkpoints(self) -> list:
-        #with open("checkpoints_track1.txt", 'r') as file:
-        with open("checkpoints_track1.txt", 'r') as file:
+        track = "checkpoints/" + self.map + ".txt"
+        with open(track, 'r') as file:
             lines = file.readlines()
             checkpoints = [ast.literal_eval(line.strip()) for line in lines]
         file.close()
         return checkpoints
     
     # easy way to get the position of the track walls
-    def _get_walls(self, track) -> np.array:
+    def _get_walls(self) -> np.array:
+        track = "track_info\\" + self.map + ".csv"
         return np.genfromtxt(track, delimiter=',', dtype = bool).reshape(1920, 1080)
