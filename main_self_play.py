e72d5498a353099e80e6bdcf2db117c209ac26d3
BitorqubitT <thierryars2@hotmail.com>
2024-12-11 21:21:59 +0100

    add comments

--- main_self_play.py
+++ main_self_play.py
@@ -1,53 +1,22 @@
+"""
+    Initializes the game environment and runs the main game loop.
+    Use this script to test the driving, track, game environment.
+"""
 import pygame
 import pygame.freetype
 from game_env import Environment
-from dataclasses import dataclass, field
-from typing import Dict, List, Tuple
-"""
-    Self play to test the game
-"""
-
-
-@dataclass
-class SpawnHolder:
-    data: Dict[str, List[Tuple[int, int, float]]] = field(default_factory = dict)
-
-    def add_data(self, key: str, value: List[Tuple[int, int, float]]):
-        self.data[key] = value
-    
-    def get_data(self, key: str) -> List[Tuple[int, int, float]]:
-                return self.data.get(key, [])
-
-spawn_holder = SpawnHolder()
-
-spawn_holder.add_data('track 1', [(550, 125, 0),
-                                (1562, 291, 1.46),
-                                (1514, 591, 1.78),
-                                (1531, 860, 1.36),
-                                (1168, 956, 3.24),
-                                (610, 942, 3.246),
-                                (239, 681, 4.39),
-                                (236, 459, 4.92),
-                                (223, 279, 4.9),
-                                (363, 129, 5.759)])
-
-
-
 
 if __name__ == "__main__":
 
     MODE = "player"
-    #TODO: fix start point
     MAP_NAMES = ["scuffed monza", "track 1"]
-    MAP = "scuffed monza"
+    MAP = "track 1"
     START_POS = "random"
     NUMBER_OF_PLAYERS = 1
+
     env = Environment(MODE, MAP, START_POS, NUMBER_OF_PLAYERS)
-
     current_game = True
-    rewardd = 0
-
-    #TODO: Change action in game_env.
+    total_reward = 0
 
     while current_game:
         for event in pygame.event.get():
@@ -59,6 +28,6 @@ if __name__ == "__main__":
         keys = pygame.key.get_pressed()
         keys = [keys]
         observation, reward, terminated, truncated = env.step(keys)[0]
-        rewardd += reward
+        total_reward += reward
         
-        print(rewardd)
+        print(total_reward)
