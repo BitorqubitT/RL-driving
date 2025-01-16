793d708c2d373c060cd2b067e1ad0e1526176180
BitorqubitT <thierryars2@hotmail.com>
2025-01-16 23:02:32 +0100

    documentation

--- dataclass_helper.py
+++ dataclass_helper.py
@@ -1,16 +1,5 @@
 """
-This module provides the SpawnHolder class for managing spawn data in the game environment.
-
-The SpawnHolder class allows loading spawn data from CSV files and retrieving
-the data for specific maps. The data is stored in a dictionary where the key
-is the map name and the value is a list of tuples containing spawn coordinates
-and angle.
-
-Example usage:
-    spawn_holder = SpawnHolder()
-    spawn_holder.load_data_from_file('map1')
-    data = spawn_holder.get_data('map1')
-
+This module provides the SpawnHolder class and Args class.
 """
 from dataclasses import dataclass, field
 from typing import Dict, List, Tuple
@@ -19,10 +8,20 @@ import itertools
 from dataclasses import dataclass, field
 from typing import List
 
-
 @dataclass
 class SpawnHolder:
     """Class for holding and managing spawn data.
+
+    The SpawnHolder class allows loading spawn data from CSV files and retrieving
+    the data for specific maps. The data is stored in a dictionary where the key
+    is the map name and the value is a list of tuples containing spawn coordinates
+    and angle.
+
+    Example usage:
+        spawn_holder = SpawnHolder()
+        spawn_holder.load_data_from_file('map1')
+        data = spawn_holder.get_spawn_data('map1')
+
 
     Attributes:
         data: A dictionary where the key is a string representing the map name,
@@ -39,9 +38,6 @@ class SpawnHolder:
                       expects a CSV file located in the 'spawn_info' directory.
                       Each row in the CSV file should contain three values: two integers and a float.
 
-        Raises:
-            FileNotFoundError: If the specified CSV file does not exist.
-            ValueError: If the CSV file contains rows that cannot be converted to the expected types.
         """
         with open("spawn_info/" + map_name + ".csv", mode='r') as file:
             reader = csv.reader(file)
@@ -51,7 +47,7 @@ class SpawnHolder:
                 else:
                     self.data[map_name] = [(int(row[0]), int(row[1]), float(row[2]))]
     
-    def get_data(self, key: str) -> List[Tuple[int, int, float]]:
+    def get_spawn_data(self, key: str) -> List[Tuple[int, int, float]]:
         """Retrieves the spawn data for a given map.
 
         Args:
